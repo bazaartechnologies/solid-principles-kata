7df6cb22f975fde18fba13e0624787639d26f06a
faizan-urfi <faizan.ahmad@bazaartech.com>
2023-08-08 14:31:33 +0500

    Updated LSP Package

--- src/main/kotlin/lsp/fakes/ZongTopup.kt
+++ src/main/kotlin/lsp/fakes/ZongTopup.kt
@@ -3,7 +3,7 @@ package lsp.fakes
 class ZongTopup : JazzTopup() {
 
     private val zongTaxDetail: Double = 20.0
-    fun getZongTaxDetails(amount: Int) {
+    fun getTaxDetails(amount: Int) {
         println("Zong tax details: $zongTaxDetail")
     }
 }
